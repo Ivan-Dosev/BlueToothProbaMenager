a1ed98836ca6e6922913ddcb20e773c0a081ad5b
Ivan Dimitrov <biobeehive@abv.bg>
2020-12-31 07:20:59 +0200

    Initial Commit

---
+++ BlueToothProbaMenager/BlueToothProbaMenagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueToothProbaMenagerApp.swift
+//  BlueToothProbaMenager
+//
+//  Created by Ivan Dimitrov on 31.12.20.
+//
+
+import SwiftUI
+
+@main
+struct BlueToothProbaMenagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlueToothProbaMenager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlueToothProbaMenager
+//
+//  Created by Ivan Dimitrov on 31.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
